36e5a9b08cfb88595e37e0d35d602fd82bd78d3f
Lisa Kuipers <lisa.l.kuipers@student.hu.nl>
2022-06-08 11:07:53 +0200

    Rcolorbrewer teruggevoegd

--- R/activity_years.R
+++ R/activity_years.R
@@ -6,7 +6,7 @@
 #' @param activity What activity you want to visualise
 #' @import magrittr
 #' @import ggplot2
-#' @import tidyverse
+#' @import RColorBrewer
 #'
 #' @return A plot
 #' @export
@@ -22,5 +22,6 @@ activity_years <- function(dataset,activity){
   dataset %>% ggplot(aes(x=month,y=dataset[[activity]], group=year, color=factor(year)))+
     geom_line()+
     scale_x_continuous(name="Month", breaks=(1:12), limits=c(1, 12))+
+    scale_color_brewer(palette = "Set1")+
     theme_minimal()
 }
