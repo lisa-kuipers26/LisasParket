651136da3ff44815e0f169cb82b4772952a87349
Lisa Kuipers <lisa.l.kuipers@student.hu.nl>
2022-06-09 00:42:39 +0200

    tweede functie

--- R/covid19_progressplot.R
+++ R/covid19_progressplot.R
@@ -1,9 +1,27 @@
-function(condition,type){
-  data_filtered %>% ggplot(aes(x=date_range, y=condition))+
+#' Function for visulaising covid cases and deaths
+#'
+#' @param dataset filtered datset with covid data
+#' @param condition choose out of cases or deaths
+#'
+#' @import magrittr
+#' @import ggplot2
+#'
+#' @return A plot with date range on x-axis and amount of deaths/cases on y axis
+#' @export
+#'
+#' @examples
+#' dataset <- data.frame(date_range=(20200512:20200812),
+#'            deaths=(1:301),cases=(1:301),
+#'            dateRep=(20200512:20200812))
+#' covid19_progressplot(dataset,"cases")
+#' covid19_progressplot(dataset,"deaths")
+covid19_progressplot <- function(dataset,condition){
+  date_range<-NULL
+  dataset %>% ggplot(aes(x=date_range, y=dataset[[condition]]))+
     geom_point()+
-    scale_x_discrete(breaks=data_filtered$date_range[seq(1,length(data_filtered$date_range),by=50)])+
+    scale_x_discrete(breaks=dataset$date_range[seq(1,length(dataset$date_range),by=50)])+
     theme(axis.text.x = element_text(angle = 90))+
     labs(x="Date",
-         y=type,
-         title = paste("Amount of COVID-19",type," from",data_filtered$dateRep[length(data_filtered$dateRep)],"to",data_filtered$dateRep[1]))
+         y=dataset[[condition]],
+         title = paste("Amount of COVID-19",condition," from",dataset$dateRep[length(dataset$dateRep)],"to",dataset$dateRep[1]))
 }
